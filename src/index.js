5c50971e62c69dbb4b6911e910df67909ff065b8
Vishal Doshi <vishal.doshi@gmail.com>
2015-11-28 00:12:44 +0530

    Replace __FILE__ with name of current file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import fs from 'fs';
+import util from 'util';
 
 const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+const name = "[magic-constants]";
 
 export default function ({ types: t }) {
   return {
-    visitor: {
+    visitor: {      
       ReferencedIdentifier(path) {
         if (path.node.name === "__VERSION__") {
           path.replaceWith(t.valueToNode(version));
+          path.hub.file.log.debug(`${name} replaced __VERSION__ with ${path.node.value}`);
         }
+      },
+      StringLiteral(path) {
+      	if (path.node.value === "__FILE__") {
+      		path.node.value = path.hub.file.opts.filename;
+      		path.hub.file.log.debug(`${name} replaced __FILE__ with ${path.node.value}`);
+      	}
       }
     }
   };
